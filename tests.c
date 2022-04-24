927cca15b5013ac95ba820b70bf7a8550ced2687
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-25 01:16:45 +0200

    tests: add tests

--- tests.c
+++ tests.c
@@ -6,39 +6,49 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 18:55:14 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/24 18:32:38 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/25 01:15:46 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+
+void	check(int id, int fd, char const *r)
+{
+	char	*gnl;
+
+	gnl = get_next_line(fd);
+	if ((r == NULL && gnl == NULL) || (r != NULL && strcmp(gnl, r) == 0))
+	{
+		printf("\e[32mTest %i: OK\n\e[00m", id);
+	}
+	if ((r == NULL && gnl != NULL) || (r != NULL && strcmp(gnl, r) != 0))
+	{
+		printf("\e[91mTest %i: KO\n\e[00m", id);
+		printf("> (%s)\n", r);
+		printf("< (%s)\n", gnl);
+	}
+	free(gnl);
+}
 
 int	main(int argc, char *argv[])
 {
-	char	*file_path;
-	char	*str;
-	int		fd;
-	int		lines;
-	int		max;
+	int	fd;
 
-	file_path = "test";
-	fd = open(file_path, O_RDWR);
-	if (fd < 0)
-		return (1);
-	lines = 0;
-	str = get_next_line(fd);
-	max = 99999;
 	if (argc == 2)
-		max = atoi(argv[1]);
-	while (str != NULL && lines < max)
-	{
-		lines++;
-		printf("%s\n", str);
-		free(str);
-		str = get_next_line(fd);
-	}
-	printf("lines %i\n", lines);
+		printf("argv %i\n", atoi(argv[1]));
+	fd = open("./tests/test1", O_RDWR);
+	check(1, fd, "ABCD");
+	check(2, fd, "EFGH");
+	check(3, fd, "IJKLMN");
+	check(4, fd, NULL);
+	close(fd);
+	check(5, -1, NULL);
+	fd = open("./tests/test2", O_RDWR);
+	check(6, fd, "\n");
+	check(7, fd, NULL);
 	close(fd);
 	return (0);
 }
