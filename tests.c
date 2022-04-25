60c8d5da6bea2d4727be0da39677307b8422b3f1
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-25 02:03:02 +0200

    tests: alter and add tests

--- tests.c
+++ tests.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 18:55:14 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/25 01:15:46 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/25 02:01:54 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,36 @@
 #include <stdio.h>
 #include <string.h>
 
+void	check_sucess(int id)
+{
+	printf("\e[32mTest %i: OK\n\e[00m", id);
+}
+
+void	check_error(int id, char const *gnl, char const *r)
+{
+	printf("\e[91mTest %i: KO\n\e[00m", id);
+	printf("> (%s)\n", r);
+	printf("< (%s)\n", gnl);
+}
+
 void	check(int id, int fd, char const *r)
 {
 	char	*gnl;
 
 	gnl = get_next_line(fd);
-	if ((r == NULL && gnl == NULL) || (r != NULL && strcmp(gnl, r) == 0))
+	if (r == NULL)
 	{
-		printf("\e[32mTest %i: OK\n\e[00m", id);
+		if (gnl == NULL)
+			check_sucess(id);
+		else
+			check_error(id, gnl, r);
 	}
-	if ((r == NULL && gnl != NULL) || (r != NULL && strcmp(gnl, r) != 0))
+	else
 	{
-		printf("\e[91mTest %i: KO\n\e[00m", id);
-		printf("> (%s)\n", r);
-		printf("< (%s)\n", gnl);
+		if (gnl != NULL && strcmp(gnl, r) == 0)
+			check_sucess(id);
+		else
+			check_error(id, gnl, r);
 	}
 	free(gnl);
 }
@@ -40,8 +56,8 @@ int	main(int argc, char *argv[])
 	if (argc == 2)
 		printf("argv %i\n", atoi(argv[1]));
 	fd = open("./tests/test1", O_RDWR);
-	check(1, fd, "ABCD");
-	check(2, fd, "EFGH");
+	check(1, fd, "ABCD\n");
+	check(2, fd, "EFGH\n");
 	check(3, fd, "IJKLMN");
 	check(4, fd, NULL);
 	close(fd);
@@ -49,6 +65,11 @@ int	main(int argc, char *argv[])
 	fd = open("./tests/test2", O_RDWR);
 	check(6, fd, "\n");
 	check(7, fd, NULL);
+	close(fd);
+	fd = open("./tests/test3", O_RDWR);
+	check(8, fd, "0123456789012345678901234567890123456789\n");
+	check(9, fd, "0");
+	check(10, fd, NULL);
 	close(fd);
 	return (0);
 }
