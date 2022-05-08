4776651400e34299a39de87a878a2aed2351d037
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-05-08 23:33:14 +0200

    tests: updated

--- tests/bonus.c
+++ tests/bonus.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 05:38:23 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/28 05:42:29 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/08 23:31:54 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,27 @@ void	bonus(void)
 	int	fd3;
 	int	fd4;
 
-	fd1 = open("./tests/test1", O_RDWR);
-	fd2 = open("./tests/test2", O_RDWR);
-	fd3 = open("./tests/test3", O_RDWR);
-	fd4 = open("./tests/test4", O_RDWR);
+	printf("\e[33mBonus\e[00m\n");
+	printf("BUFFER_SIZE %d\n", BUFFER_SIZE);
+	fd1 = open("test1", O_RDWR);
+	fd2 = open("test2", O_RDWR);
+	fd3 = open("test3", O_RDWR);
+	fd4 = open("test4", O_RDWR);
 	check(1, get_next_line(fd1), "ABCD\n");
-	check(5, get_next_line(fd2), "\n");
-	check(2, get_next_line(fd1), "EFGH\n");
-	check(7, get_next_line(fd3), "0123456789012345678901234567890123456789\n");
-	check(3, get_next_line(fd1), "IJKLMN");
-	check(4, get_next_line(fd1), NULL);
-	check(11, get_next_line(fd4), "\n");
-	check(6, get_next_line(fd2), NULL);
-	check(12, get_next_line(fd4), "\n");
-	check(15, get_next_line(1000), NULL);
-	check(8, get_next_line(fd3), "0");
-	check(9, get_next_line(fd3), NULL);
-	check(10, get_next_line(fd4), "\n");
-	check(13, get_next_line(fd4), NULL);
-	check(14, get_next_line(-1), NULL);
+	check(2, get_next_line(fd2), "\n");
+	check(3, get_next_line(fd1), "EFGH\n");
+	check(4, get_next_line(fd3), "0123456789012345678901234567890123456789\n");
+	check(5, get_next_line(fd1), "IJKLMN");
+	check(6, get_next_line(fd1), NULL);
+	check(7, get_next_line(fd4), "\n");
+	check(8, get_next_line(fd2), NULL);
+	check(9, get_next_line(fd4), "\n");
+	check(10, get_next_line(1000), NULL);
+	check(11, get_next_line(fd3), "0");
+	check(12, get_next_line(fd3), NULL);
+	check(13, get_next_line(fd4), "\n");
+	check(14, get_next_line(fd4), NULL);
+	check(15, get_next_line(-1), NULL);
 	close(fd1);
 	close(fd2);
 	close(fd3);
--- tests/mandatory.c
+++ tests/mandatory.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 05:38:26 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/28 05:40:56 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/05/08 23:31:50 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,24 @@ void	mandatory(void)
 {
 	int	fd;
 
-	fd = open("./tests/test1", O_RDWR);
+	printf("\e[33mMandatory\e[00m\n");
+	printf("BUFFER_SIZE %d\n", BUFFER_SIZE);
+	fd = open("test1", O_RDWR);
 	check(1, get_next_line(fd), "ABCD\n");
 	check(2, get_next_line(fd), "EFGH\n");
 	check(3, get_next_line(fd), "IJKLMN");
 	check(4, get_next_line(fd), NULL);
 	close(fd);
-	fd = open("./tests/test2", O_RDWR);
+	fd = open("test2", O_RDWR);
 	check(5, get_next_line(fd), "\n");
 	check(6, get_next_line(fd), NULL);
 	close(fd);
-	fd = open("./tests/test3", O_RDWR);
+	fd = open("test3", O_RDWR);
 	check(7, get_next_line(fd), "0123456789012345678901234567890123456789\n");
 	check(8, get_next_line(fd), "0");
 	check(9, get_next_line(fd), NULL);
 	close(fd);
-	fd = open("./tests/test4", O_RDWR);
+	fd = open("test4", O_RDWR);
 	check(10, get_next_line(fd), "\n");
 	check(11, get_next_line(fd), "\n");
 	check(12, get_next_line(fd), "\n");
